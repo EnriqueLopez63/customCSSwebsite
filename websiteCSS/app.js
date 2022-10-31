02a0a0ecefbd6dd8baff6db32ec7b16b33a6a96d
EnriqueLopez63 <111899612+EnriqueLopez63@users.noreply.github.com>
2022-10-31 14:55:46 -0500

    Add files via upload

---
+++ websiteCSS/app.js
@@ -0,0 +1,11 @@
+function modalOn() {
+    document.getElementById("modal-text").style.display = "block";
+    document.getElementById("modal-button").style.display = "none";
+}
+
+    
+    
+function modalOff() {
+    document.getElementById("modal-text").style.display = "none";
+    document.getElementById("modal-button").style.display = "block";
+}
